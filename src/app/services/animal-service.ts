d48362b88448d42d68a3d477ec2a6a540a15c1df
Sebastian <Sebastian@gmail.com>
2026-04-22 14:07:03 -0500

    Se agregan mas variables

--- src/app/services/animal-service.ts
+++ src/app/services/animal-service.ts
@@ -1,6 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 @Injectable({
   providedIn: 'root',
 })
-export class AnimalService {}
+export class AnimalService {
+constructor(private http: HttpClient){}
+apiUri = '/api/animals';
+httpOptions = new HttpHeaders().set('Content-Type', 'application/json');
+getAllAnimalsData(): Observable<any> {
+ return this.http.get<any>(this.apiUri)
+}
+
+}
