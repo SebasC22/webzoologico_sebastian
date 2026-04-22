f41bf8b4e5b5d93204c64e65bcea7845605f7902
Sebastian <Sebastian@gmail.com>
2026-04-22 14:15:39 -0500

    Se agregan par de cositas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { AnimalComponent } from './components/animal-component/animal-component';
+export const routes: Routes = [
+    { path: 'inicio', component: AnimalComponent }
+];
--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnimalService } from '../../services/animal-service';
 
 @Component({
   selector: 'app-animal-component',
@@ -6,4 +7,18 @@ import { Component } from '@angular/core';
   templateUrl: './animal-component.html',
   styleUrl: './animal-component.css',
 })
-export class AnimalComponent {}
+export class AnimalComponent {
+animalList:any= [];
+
+ constructor(private animalService:AnimalService) {}
+getAllAnimals() {
+ this.animalService.getAllAnimalsData().subscribe((data: {}) => {
+ this.animalList=data;
+ });
+ }
+ ngOnInit() {
+ this.getAllAnimals();
+ }
+
+
+}
