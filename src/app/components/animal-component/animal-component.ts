512c45c45558b104b84179d4207cf9c068d62d7c
Sebastian <Sebastian@gmail.com>
2026-05-06 13:25:10 -0500

    SE AGREGAN

--- src/app/components/animal-component/animal-component.ts
+++ src/app/components/animal-component/animal-component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnChanges, ChangeDetectorRef } from '@angular/core';
 import { AnimalService } from '../../services/animal-service';
-
+import { CommonModule } from '@angular/common';
+ 
 @Component({
   selector: 'app-animal-component',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './animal-component.html',
-  styleUrl: './animal-component.css',
+  styleUrls: ['./animal-component.css'],
 })
-export class AnimalComponent {
-animalList:any= [];
-
- constructor(private animalService:AnimalService) {}
-getAllAnimals() {
- this.animalService.getAllAnimalsData().subscribe((data: {}) => {
- this.animalList=data;
- });
- }
- ngOnInit() {
- this.getAllAnimals();
- }
-
-
+export class AnimalComponent implements OnInit, OnChanges {
+ 
+  animalList: any[] = [];
+ 
+  constructor(
+    private animalService: AnimalService,
+    private cd: ChangeDetectorRef
+  ) {}
+ 
+  getAllAnimals() {
+    this.animalService.getAllAnimalsData().subscribe((data: any) => {
+      this.animalList = data;
+      this.cd.detectChanges();
+    });
+  }
+ 
+  ngOnInit() {
+    this.getAllAnimals();
+  }
+ 
+  ngOnChanges() {
+    this.getAllAnimals();
+  }
 }
